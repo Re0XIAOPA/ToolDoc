910c5691639b5a14be863443467ae2fd7c773b6f
MajorFoxs_OvO <yemiao_0@outlook.com>
2025-02-09 17:07:07 +0800

    修改

--- docs/.vitepress/cache/theme/index.js
+++ docs/.vitepress/cache/theme/index.js
@@ -1,34 +1,19 @@
-// https://vitepress.dev/guide/custom-theme
-import {
-  h
-} from 'vue'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import DefaultTheme from 'vitepress/theme';
 import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-import {
-  useRoute
-} from 'vitepress';
+import { useRoute } from 'vitepress';
 
-/** @type {import('vitepress').Theme} */
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp(ctx);
-    // 注册全局组件，如果你不想使用也可以不添加
-    ctx.app.component('vImageViewer', vImageViewer);
-    // ...
-  },
-  setup() {
-    // 获取路由
-    const route = useRoute();
-    // 使用
-    imageViewer(route);
-  }
-}
+    ...DefaultTheme,
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+        // 注册全局组件（可选）
+        ctx.app.component('vImageViewer', vImageViewer);
+    },
+    setup() {
+        const route = useRoute();
+        // 启用插件
+        imageViewer(route);
+    }
+};
